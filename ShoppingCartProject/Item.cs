bcc311705d15c33ffd974978846acc1a65602826
artig <artig@maveric-systems.com>
2022-12-27 11:43:28 +0530

    ShoppingCart Program

---
+++ ShoppingCartProject/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ShoppingCart shop=new ShoppingCart();
+            ShoppingCart shop1 = new ShoppingCart();
+
+            shop.id = 101;
+            shop.descr = " Laptop";
+            shop.quantity = 5;
+            shop.price = 45000;
+
+            shop1.id = 102;
+            shop1.descr = " Laptop";
+            shop1.quantity = 2;
+            shop1.price = 55000;
+
+            shop.PrintItemDetails();
+            shop1.PrintItemDetails();
+
+            shop.PrintItem();
+            shop1.PrintItem();
+        }
+    }
+}
---
+++ ShoppingCartProject/ShoppingCart.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartProject
+{
+    public class ShoppingCart
+    {
+       public int id;
+        public string descr;
+        public int quantity;
+        public double price;
+
+        public void PrintItemDetails()
+        {
+            Console.WriteLine("id : " + id);
+            Console.WriteLine("descr : " + descr);
+            Console.WriteLine("quantity : " + quantity);
+            Console.WriteLine("price : " + price);
+            Console.WriteLine("--------------------------------------------------");
+        }
+
+        public  void PrintItem()
+        {
+            double final_price;
+            if(quantity==2)
+            {
+                final_price = (price-(price * 10 / 100)) * quantity;
+                Console.WriteLine("Congrats you have get 10% discount :" +final_price);
+            }
+            else
+            
+                if(quantity>=3&&quantity<5)
+                {
+                    final_price = (price - (price * 15 / 100)) * quantity;
+                    Console.WriteLine("Congrats you have get 15% discount :" + final_price);
+                }
+                else
+                
+                    if(quantity==5)
+                    {
+                        final_price = (price - (price * 25 / 100) ) * quantity;
+                        Console.WriteLine("Congrats you have get 25% discount :" + final_price);
+                    }
+
+                }
+            }
+        
+
+    
+}
